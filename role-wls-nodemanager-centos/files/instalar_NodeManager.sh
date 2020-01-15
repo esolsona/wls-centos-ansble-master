ff27e8c43650ddfcce583dbf65c3874527a4e609
esolsona <enric.solsona@gmail.com>
2020-01-15 14:52:57 +0100

    Update instalar_NodeManager.sh

--- role-wls-nodemanager-centos/files/instalar_NodeManager.sh
+++ role-wls-nodemanager-centos/files/instalar_NodeManager.sh
@@ -2,7 +2,8 @@
 
 # Load Environment
 export CONFIG_JVM_ARGS=-Djava.security.egd=file:/dev/./urandom
-. $WLS_HOME/server/bin/setWLSEnv.sh
+#. $WLS_HOME/server/bin/setWLSEnv.sh
+. /opt/oracle/weblogic/product/12.2.1/wlserver/server/bin/setWLSEnv.sh
 
 # Create the domain.
 java weblogic.WLST /tmp/create_nodemanagr_ref.py -p /tmp/myDomain.properties
