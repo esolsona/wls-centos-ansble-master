ed528d4ca90f2d231cdbcd92f3ce56064ff50f7a
esolsona <enric.solsona@gmail.com>
2020-01-15 14:47:21 +0100

    Update instalar_cluster.sh

--- role-wls-admin-centos/files/instalar_cluster.sh
+++ role-wls-admin-centos/files/instalar_cluster.sh
@@ -2,7 +2,8 @@
 
 # Load Envi ronment
 export CONFIG_JVM_ARGS=-Djava.security.egd=file:/dev/./urandom
-. $WLS_HOME/server/bin/setWLSEnv.sh
+#. $WLS_HOME/server/bin/setWLSEnv.sh
+. /opt/oracle/weblogic/product/12.2.1/wlserver/server/bin/setWLSEnv.sh
 
 # Create the domain.
 java weblogic.WLST /tmp/create_cluster.py -p /tmp/myDomain.properties
